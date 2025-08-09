396559074197baf60bada6d9f3e8fa42300eb43d
Ouamouno <jeankelouaouamoun71@gmail.com>
2025-08-09 19:31:26 +0000

    deploiement correction

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import "../styles/tailwind-force.css";
 import NavigationMenu from "@/components/Layout/NavigationMenu";
 import Footer from "@/components/Layout/Footer";
 
